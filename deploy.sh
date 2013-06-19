c3e0e95a662235c2f361c2b56c6a0e1291de0527
sawada naoya <naoya@kinneas.tuntunkun.org>
2013-06-19 18:10:38 +0900

    デプロイスクリプトの作成

---
+++ deploy.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+function check_uid()
+{
+	if [ $EUID -ne 0 ]; then
+  	echo "root 権限で実行してください"
+		return 1
+	fi
+}
+
+function deploy()
+{
+	check_uid || return
+
+	# install required packages...
+	if ! rpm -q python-setuptools >/dev/null 2>&1; then
+		yum -y install python-setuptools
+	fi
+	
+	# install pip
+	easy_install pip
+
+	# install virtualenv
+	pip install virtualenv
+}
+
+
+function virtdeploy()
+{
+	# create vitual enviroment
+	virtualenv  --no-site-package .
+
+	# enter vitual enviroment
+	source ./bin/activate
+
+}
+
+function usage()
+{
+	cat <<EOF
+Usage:
+	# デプロイ
+	sudo ./stiercmd deploy
+	sudo ./stiercmd virtdeploy
+EOF
+}
+
+case "$1" in
+	"deploy") deploy $@;;
+	"virtdeploy") virtdeploy $@;;
+	*) usage;;
+esac
+
+# vim: set nu ts=2 autoindent : 
